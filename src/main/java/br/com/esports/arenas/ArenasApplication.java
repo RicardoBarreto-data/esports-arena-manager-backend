f2e16279c7ab3a49ee22ffb88e04411a8d72741a
Ricardo Barreto <ricardobarreto687@gmail.com>
2026-02-11 17:45:49 -0300

    Etapa 9 - Estrutura inicial configurada. Spring Boot + MySQL integrados com sucesso.

---
+++ src/main/java/br/com/esports/arenas/ArenasApplication.java
@@ -0,0 +1,13 @@
+package br.com.esports.arenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenasApplication.class, args);
+	}
+
+}

040e3cfe82a4203f09e4fe1a3528caf7379d9722
Ricardo Barreto <ricardobarreto687@gmail.com>
2026-02-13 18:56:24 -0300

    Camada CRUD completa para entidade de Ranking.

---
+++ src/main/java/br/com/esports/arenas/controller/RankingController.java
@@ -0,0 +1,52 @@
+package br.com.esports.arenas.controller;
+
+import br.com.esports.arenas.model.Ranking;
+import br.com.esports.arenas.service.RankingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rankings")
+public class RankingController {
+
+    private final RankingService rankingService;
+
+    public RankingController(RankingService rankingService) {
+        this.rankingService = rankingService;
+    }
+
+    @GetMapping
+    public List<Ranking> listar() {
+        return rankingService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ranking> buscar(@PathVariable Integer id) {
+        return rankingService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/torneio/{torneioId}")
+    public List<Ranking> listarPorTorneio(@PathVariable Integer torneioId) {
+        return rankingService.listarPorTorneio(torneioId);
+    }
+
+    @GetMapping("/time/{timeId}")
+    public List<Ranking> listarPorTime(@PathVariable Integer timeId) {
+        return rankingService.listarPorTime(timeId);
+    }
+
+    @PostMapping
+    public Ranking criar(@RequestBody Ranking ranking) {
+        return rankingService.salvar(ranking);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        rankingService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
